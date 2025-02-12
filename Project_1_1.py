14da798d9c682fe0cd31c6eee0ccd0e7f8ebaece
arothkowitz <alec@jasonvision.com>
2025-02-12 15:17:10 +0100

    Reorganized codes into methods for calculating and graphing

--- Project_1_1.py
+++ Project_1_1.py
@@ -6,27 +6,34 @@ import os
 import seaborn as sns
 
 fitbit = pd.read_csv("data/daily_activity.csv")
-users = pd.unique(fitbit['Id'])
-print("Total number of users: " + str(len(users)))
 
-data = []
-for user in users:
-  data.append(fitbit[(fitbit['Id'] == user)]['TotalDistance'].sum())
 
-'''
-bins = np.arange(0, max(data) + 5, 15)
-plt.hist(data, bins=bins, edgecolor='black')  # Adjust bins for grouping
-plt.xlabel("Distance Walked (km)")
-plt.ylabel("Frequency")
-plt.title("Histogram Plot of Walking Distances")
+def calc_num_users(fitbit):
+  users = pd.unique(fitbit['Id'])
+  print("Total number of users: " + str(len(users)))
 
-plt.show()
-'''
+def visualize_data(file_name):
+  users = pd.unique(fitbit['Id'])
+  data = []
+  for user in users:
+    data.append(fitbit[(fitbit['Id'] == user)]['TotalDistance'].sum())
+  '''
+  bins = np.arange(0, max(data) + 5, 15)
+  plt.hist(data, bins=bins, edgecolor='black')  # Adjust bins for grouping
+  plt.xlabel("Distance Walked (km)")
+  plt.ylabel("Frequency")
+  plt.title("Histogram Plot of Walking Distances")
 
-sns.kdeplot(data, fill=True, color="blue", clip =(0, None), bw_adjust=.5)
+  plt.show()
+  '''
 
-plt.xlabel("Distance Walked (km)")
-plt.ylabel("Density")
-plt.title("Density Plot of Walking Distances")
+  sns.kdeplot(data, fill=True, color="blue", clip =(0, None), bw_adjust=.5)
 
-plt.show()
+  plt.xlabel("Distance Walked (km)")
+  plt.ylabel("Density")
+  plt.title("Density Plot of Walking Distances")
+
+  plt.show()
+
+calc_num_users(fitbit)
+visualize_data(fitbit)
