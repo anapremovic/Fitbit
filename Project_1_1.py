0281384e9430bbf4c7999f3290fe0fd29a6dc55d
arothkowitz <alec@jasonvision.com>
2025-02-14 13:55:02 +0100

    Created a histogram_visualization method and added comments

--- Project_1_1.py
+++ Project_1_1.py
@@ -9,31 +9,44 @@ fitbit = pd.read_csv("data/daily_activity.csv")
 
 
 def calc_num_users(fitbit):
+  """Calculates the total number of unique users in the dataset"""
   users = pd.unique(fitbit['Id'])
-  print("Total number of users: " + str(len(users)))
+  return len(users)
 
-def visualize_data(file_name):
+def visualize_data(fitbit):
+  """Create a density plot of the total distance walked by individuals"""
   users = pd.unique(fitbit['Id'])
   data = []
   for user in users:
     data.append(fitbit[(fitbit['Id'] == user)]['TotalDistance'].sum())
-  '''
-  bins = np.arange(0, max(data) + 5, 15)
-  plt.hist(data, bins=bins, edgecolor='black')  # Adjust bins for grouping
-  plt.xlabel("Distance Walked (km)")
-  plt.ylabel("Frequency")
-  plt.title("Histogram Plot of Walking Distances")
-
-  plt.show()
-  '''
-
-  sns.kdeplot(data, fill=True, color="blue", clip =(0, None), bw_adjust=.5)
+  
+  #Clip the data at 0 as there are no users walking less than 0 kilometers
+  #Reduce the bandwidth to prevent smoothing and create a more representative plot
+  sns.kdeplot(data, fill=True, color="blue", clip =(0, None), bw_adjust=1)
 
   plt.xlabel("Distance Walked (km)")
   plt.ylabel("Density")
   plt.title("Density Plot of Walking Distances")
 
   plt.show()
+  
+def histogram_visualization(fitbit):
+  """Create a histogram describing the frequency
+    of the total number of steps walked by individuals"""
+  users = pd.unique(fitbit['Id'])
+  data = []
+  for user in users:
+    data.append(fitbit[(fitbit['Id'] == user)]['TotalDistance'].sum())
+  bins = np.arange(0, max(data), 15)
+  plt.hist(data, bins=bins, edgecolor='black') 
+  plt.xticks(bins)
+  plt.xlim(min(data), max(bins))
+  plt.xlabel("Distance Walked (km)")
+  plt.ylabel("Frequency")
+  plt.title("Histogram Plot of Walking Distances")
+
+  plt.show()
 
 calc_num_users(fitbit)
 visualize_data(fitbit)
+histogram_visualization(fitbit)
