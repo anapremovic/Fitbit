82b4cd7913b76189524af44bf3304522f7017ea6
Luke Olender <luke.olender@gmail.com>
2025-02-14 17:05:23 +0100

    Changed relative reference to data file to absolute reference

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import part1 as part1
+import os
 
 # PART 1
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_location = os.path.join(project_root, "data/daily_activity.csv")
 
-daily_activity = pd.read_csv("./data/daily_activity.csv")
+daily_activity = pd.read_csv(file_location)
 
 part1.generate_day_of_week_frequency_plot(daily_activity)
 
