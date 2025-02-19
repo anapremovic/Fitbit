7dc6438451a59d298d43c0072b0c79a81a1a8b67
arothkowitz <alec@jasonvision.com>
2025-02-19 15:12:37 +0100

    Included information for sleep sessions that occur over 2 days

--- scripts/database.py
+++ scripts/database.py
@@ -9,7 +9,7 @@ cursor = connection.cursor()
 
 
 def get_sleep_data():
-    query = f"SELECT substr(date, 1, instr(date, ' ') - 1) AS date, Id, COUNT(*) AS minutesSlept FROM minute_sleep GROUP BY logId"
+    query = f"SELECT GROUP_CONCAT(DISTINCT substr(date, 1, instr(date, ' ') - 1)) AS date, Id, COUNT(*) AS minutesSlept FROM minute_sleep GROUP BY logId"
     cursor.execute(query)
     rows = cursor.fetchall()
     sleep_data = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
