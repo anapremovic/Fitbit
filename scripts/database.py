60f8aaf775579d95f965dce190788af45784d8c5
apa109 <apa109@sfu.ca>
2025-02-19 14:54:17 +0100

    Implement function fetch the total active minutes per user and date

--- scripts/database.py
+++ scripts/database.py
@@ -1,6 +1,20 @@
 import sqlite3
 import os
+import pandas as pd
 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 file_location = os.path.join(project_root, "data/fitbit_database.db")
 connection = sqlite3.connect(file_location)
+cursor = connection.cursor()
+
+def fetch_total_active_minutes_per_user_and_date() -> pd.DataFrame:
+    query = """
+    SELECT Id, ActivityDate, SUM(VeryActiveMinutes + FairlyActiveMinutes + LightlyActiveMinutes) AS TotalActiveMinutes
+    FROM daily_activity
+    GROUP BY Id, ActivityDate
+    ORDER By Id, ActivityDate;
+    """
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
