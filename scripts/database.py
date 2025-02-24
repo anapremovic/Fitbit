7b2c416d11bde7904a4a479792efaa217438139f
arothkowitz <alec@jasonvision.com>
2025-02-24 15:46:28 +0100

    Deleted get_sleep_data() to make compatable with Ana's updates

--- scripts/database.py
+++ scripts/database.py
@@ -8,17 +8,6 @@ connection = sqlite3.connect(file_location)
 cursor = connection.cursor()
 
 
-
-def get_sleep_data():
-    query = f"SELECT GROUP_CONCAT(DISTINCT substr(date, 1, instr(date, ' ') - 1)) AS date, Id, COUNT(*) AS minutesSlept FROM minute_sleep GROUP BY logId"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    sleep_data = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
-    sleep_data.loc[:, 'Id'] = sleep_data.loc[:, 'Id'].astype(int)
-    sleep_data.loc[:, 'minutesSlept'] = sleep_data.loc[:, 'minutesSlept'].astype(int)
-    return sleep_data
-
-
 def get_daily_steps():
     query = f"SELECT Id, ActivityDate, TotalSteps FROM daily_activity"
     cursor.execute(query)
