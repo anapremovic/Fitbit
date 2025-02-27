1a63d704c98fa8d0926de5a1a4cf908bde676f39
apa109 <apa109@sfu.ca>
2025-02-27 12:25:28 +0100

    Add Part 4 file

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 import part1 as part1
 import part3 as part3
+import part4 as part4
 
 part1.execute_part_1()
 part3.execute_part_3()
+part4.execute_part_4()
---
+++ scripts/part4.py
@@ -0,0 +1,3 @@
+def execute_part_4():
+    """Generate all visualizations for part 4 of the project."""
+
