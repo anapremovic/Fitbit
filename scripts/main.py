e0188fa544c312a169bf6a75565ac37b863ac822
apa109 <apa109@sfu.ca>
2025-03-02 15:01:39 +0100

    Make variable names look nicer in main script

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
-import part1 as part1
-import part3 as part3
-import part4 as part4
+import part1 as p1
+import part3 as p3
+import part4 as p4
 
-part1.execute_part_1()
-part3.execute_part_3()
-part4.execute_part_4()
+p1.execute_part_1()
+p3.execute_part_3()
+p4.execute_part_4()
