8b09ca7336a4d65b4dde02d6285502770b76d4c4
Lucas Lee <lucasdlee23@gmail.com>
2025-03-07 21:40:40 +0100

    Squashed commit of the following:
    commit 1830af797efac1ef90420cd55887a0389b3f3f86
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 21:09:38 2025 +0100
    
        Added missing key
    
    commit 5172731e994000b75365387f55e9ce0be0706712
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 21:04:40 2025 +0100
    
        Instantiate FitbitDatabase, cache result, share through st.session_state
    
    commit 885edc907c414fd53f632d4d5c32f6e6d3a8dcd0
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 20:45:28 2025 +0100
    
        Added user ID dropdown to sidebar
    
    commit 097cc7b1b1aa980530f28f97ff429f161b50b2b8
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 20:23:43 2025 +0100
    
        Added get_all_user_ids function db function
    
    commit 20f90eb89a62dbfc2bc1c000293628bae24079d7
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 20:23:23 2025 +0100
    
        Renamed dashboard page files
    
    commit 40c15b122ea943510e292edd36c3abcbdcfd4a3c
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 20:02:00 2025 +0100
    
        Removed page/sidebar titles
    
    commit 52d28dc86986bb667c799e780370126de64ab0d8
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Fri Mar 7 10:26:37 2025 +0100
    
        Removed unnecessary imports
    
    commit a2db301aeca3d10dfdb9eb8c8da58417b9c7896c
    Merge: 316f209 6d740f8
    Author: Luke Olender <64585865+LukeOlender@users.noreply.github.com>
    Date:   Fri Mar 7 10:17:48 2025 +0100
    
        Merge database-class into dashboard-boilerplate
    
        Database class
    
    commit 6d740f8a9ef6abed5a32412ae328961db90ba2ba
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 19:19:17 2025 +0100
    
        Changed referencing to csv data files
    
    commit aa45c7cac24c2400487bb346bec1340ee52f6aaf
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 19:10:48 2025 +0100
    
        Created Part4 class
    
    commit 3bc6dd24d4789c0605eb1c411469c35b53c2d7b1
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 19:02:47 2025 +0100
    
        Created Part1 class
    
    commit 988aa14346f525d7adf1f0c8e35a3482c22ac536
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 18:46:49 2025 +0100
    
        Created Part3 class
    
    commit 4eb68ece21a244f2abd91f07ea1d1e392d7a6350
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 18:46:06 2025 +0100
    
        Created FitbitDatabase class
    
    commit 316f20972c5051f6e9efb653859e958151b8bfa3
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 17:11:22 2025 +0100
    
        Added project root to PATH
    
    commit 828a941c47c60808a5c9dd1134723c866bd14841
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 16:46:48 2025 +0100
    
        Removed file references part3
    
    commit a58fdc35eb266456d3377afc77436364da46d55a
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 16:40:43 2025 +0100
    
        Fixed file reference part1
    
    commit e30d3c8f96576208bb19280498efa76a614bbb4b
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 14:01:18 2025 +0100
    
        Removed print and comments
    
    commit d8cf0c7e0e17ad5ad15f6abb87774d2e6b6f9101
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 13:59:06 2025 +0100
    
        Removed unintended line
    
    commit 591557dd56274b00b00fd34a3281f4dfa1ee5a7f
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 13:57:51 2025 +0100
    
        Initial basic page structure
    
    commit c1f9c7c5659d67c86d8fcd17b9a1e22f570f57b2
    Author: Luke Olender <luke.olender@gmail.com>
    Date:   Thu Mar 6 12:40:42 2025 +0100
    
        Incorporated new file structure

--- scripts/dashboard/pages/home.py
+++ scripts/dashboard/pages/home.py
@@ -19,18 +19,9 @@ sys.path.append(project_root)
 
 # Imports must go after setting up project root
 from scripts.database import FitbitDatabase
-from scripts.dashboard import components as ct
 
 db_location = os.path.join(project_root, "data/fitbit_database.db")
 fitbit_database = FitbitDatabase(db_location)
 
 st.header("Home")
 st.sidebar.header("Home")
-
-dashboard = ct.DashboardComponents(fitbit_database)
-selected_user = dashboard.user_id_sidebar()
-
-
-# Display selected user
-if selected_user:
-    st.write(f"### Selected User: {selected_user}")
