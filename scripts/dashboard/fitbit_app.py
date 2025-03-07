5172731e994000b75365387f55e9ce0be0706712
Luke Olender <luke.olender@gmail.com>
2025-03-07 21:04:40 +0100

    Instantiate FitbitDatabase, cache result, share through st.session_state

--- scripts/dashboard/fitbit_app.py
+++ scripts/dashboard/fitbit_app.py
@@ -11,6 +11,33 @@ import streamlit as st
 
 from scripts.database import FitbitDatabase
 
+@st.cache_resource
+def get_fitbit_db_instance() -> FitbitDatabase:
+    """Creates an instance of FitbitDatabase and, with that, establishes a connection
+    to fitbit_database.db. The result should be saved to st.session_state so that 
+    other pages can easily access this same instance.
+     
+    The decorator @st.cache_resource ensures the result is cached, so that this code is 
+    only run once on startup.
+    """
+
+    db_location = os.path.join(project_root, "data/fitbit_database.db")
+    return FitbitDatabase(db_location)
+
+st.session_state["fitbit_db"] = get_fitbit_db_instance()
+
+@st.cache_data
+def get_all_user_ids() -> tuple[str]:
+    fitbit_db: FitbitDatabase = st.session_state[""]
+    users = fitbit_db.get_all_user_ids()
+    return tuple(users.loc[:, "Id"])
+
+st.sidebar.selectbox(
+        "User ID",
+        ("All",) +  get_all_user_ids(),
+        key = "selected_user"
+    )
+
 home_page = st.Page("home_page.py", title="Home", icon="📌")
 exercise_page = st.Page("exercise_page.py", title="Exercise", icon="🏋️")
 health_page = st.Page("health_page.py", title="Health", icon="❤️")
@@ -20,16 +47,5 @@ pg = st.navigation([
     exercise_page, 
     health_page
 ])
-
-db_location = os.path.join(project_root, "data/fitbit_database.db")
-fitbit_db = FitbitDatabase(db_location)
-
-users = fitbit_db.get_all_user_ids()
-users = tuple(users.loc[:, "Id"])
-st.sidebar.selectbox(
-    "User ID",
-    ("All",) +  users,
-    key = "selected_user"
-)
 
 pg.run()
--- scripts/dashboard/home_page.py
+++ scripts/dashboard/home_page.py
@@ -1,1 +1,5 @@
 import streamlit as st
+
+from scripts.database import FitbitDatabase
+
+fitbit_db: FitbitDatabase = st.session_state["fitbit_db"]
