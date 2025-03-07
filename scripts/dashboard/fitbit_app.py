885edc907c414fd53f632d4d5c32f6e6d3a8dcd0
Luke Olender <luke.olender@gmail.com>
2025-03-07 20:45:28 +0100

    Added user ID dropdown to sidebar

--- scripts/dashboard/fitbit_app.py
+++ scripts/dashboard/fitbit_app.py
@@ -1,4 +1,15 @@
+# ----------
+# Ensures that all Python files can be imported from any location
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+sys.path.append(project_root)
+# ----------
+
 import streamlit as st
+
+from scripts.database import FitbitDatabase
 
 home_page = st.Page("home_page.py", title="Home", icon="📌")
 exercise_page = st.Page("exercise_page.py", title="Exercise", icon="🏋️")
@@ -9,5 +20,16 @@ pg = st.navigation([
     exercise_page, 
     health_page
 ])
+
+db_location = os.path.join(project_root, "data/fitbit_database.db")
+fitbit_db = FitbitDatabase(db_location)
+
+users = fitbit_db.get_all_user_ids()
+users = tuple(users.loc[:, "Id"])
+st.sidebar.selectbox(
+    "User ID",
+    ("All",) +  users,
+    key = "selected_user"
+)
 
 pg.run()
