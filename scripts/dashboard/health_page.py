c16ce850f04b0378a2784d1b9f80b91aaabed3dc
apa109 <apa109@sfu.ca>
2025-03-12 13:21:13 +0100

    Move filters to top of script

--- scripts/dashboard/health_page.py
+++ scripts/dashboard/health_page.py
@@ -4,8 +4,9 @@ import pandas as pd
 from scripts.dashboard.diagrams.health_diagrams import HealthDiagrams
 
 health_diagrams = HealthDiagrams()
-fig = health_diagrams.sleep_quantity_over_time(st.session_state["selected-user"],
-                                               pd.to_datetime(st.session_state["selected-start-date"]),
-                                               pd.to_datetime(st.session_state["selected-end-date"]))
-st.pyplot(fig)
+user = st.session_state["selected-user"]
+start_date = pd.to_datetime(st.session_state["selected-start-date"])
+end_date = pd.to_datetime(st.session_state["selected-end-date"])
 
+fig = health_diagrams.sleep_quantity_over_time(user, start_date, end_date)
+st.pyplot(fig)
