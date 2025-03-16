feded03476bb0cc711ec1b8c59d3f411e09a34bd
apa109 <apa109@sfu.ca>
2025-03-16 11:36:05 +0100

    Add subheaders

--- scripts/dashboard/health_page.py
+++ scripts/dashboard/health_page.py
@@ -41,13 +41,14 @@ heart_rate_over_time, average_heart_rate = health_diagrams.get_heart_rate_over_t
 
 # Show on dashboard
 
-# Sleep
+st.subheader("Sleep Duration")
 line, bar = st.columns(2)
 with line:
     st.plotly_chart(sleep_duration_over_time, key="sleep_duration_over_time")
 with bar:
     st.plotly_chart(sleep_duration_per_time_blocks, key="sleep_duration_per_time_blocks")
 
+st.subheader("What Affects Sleep")
 with st.container(border=True):
     weekdays, weekends = st.columns(2)
     with weekdays:
@@ -58,14 +59,14 @@ with st.container(border=True):
 
 st.plotly_chart(sedentary_hrs_to_sleep_hrs, key="sedentary_hrs_to_sleep_hrs")
 
-# Calories
+st.subheader("Calories")
 line, bar = st.columns(2)
 with line:
     st.plotly_chart(calories_burned_over_time, key="calories_burned_over_time")
 with bar:
     st.plotly_chart(calories_burned_per_time_blocks, key="calories_burned_per_time_blocks")
 
-# Heart Rate
+st.subheader("Heart Rate")
 plot, number = st.columns([3, 1])
 with plot:
     st.plotly_chart(heart_rate_over_time, key="heart_rate_over_time")
