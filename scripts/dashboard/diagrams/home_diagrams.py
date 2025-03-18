ba628497cd1f5c5b42ba1af85e0d837d9984976d
Luke Olender <luke.olender@gmail.com>
2025-03-18 23:28:05 +0100

    Added Steps and Active barplots

--- scripts/dashboard/diagrams/home_diagrams.py
+++ scripts/dashboard/diagrams/home_diagrams.py
@@ -1,8 +1,11 @@
 import plotly.graph_objects as go
+import plotly.express as px
 
 from scripts.database import FitbitDatabase
 
 PRIMARY_COLOR = "#06B0B8"
+MEDIUM_FONT_SIZE = 25
+LARGE_FONT_SIZE = 50
 
 class HomeDiagrams:
     def __init__(self, fitbit_db: FitbitDatabase):
@@ -13,8 +16,8 @@ class HomeDiagrams:
         fig = go.Figure(go.Indicator(
             mode="number",
             value=duration_days,
-            title={"text": "Days tracked", "font": {"size": 30, "color": PRIMARY_COLOR}},
-            number={"font": {"size": 50}}#, "suffix": " days"} # Change color later
+            title={"text": "Days tracked", "font": {"size": MEDIUM_FONT_SIZE, "color": PRIMARY_COLOR}},
+            number={"font": {"size": LARGE_FONT_SIZE}}#, "suffix": " days"} # Change color later
         ))
 
         fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
@@ -27,9 +30,9 @@ class HomeDiagrams:
             value=len(self.fitbit_db.user_ids),
             title={
                 "text": "Participants",
-                "font": {"size": 30, "color": PRIMARY_COLOR}
+                "font": {"size": MEDIUM_FONT_SIZE, "color": PRIMARY_COLOR}
             },
-            number={"font": {"size": 50}} # Change color later
+            number={"font": {"size": LARGE_FONT_SIZE}} # Change color later
         ))
 
         fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
@@ -51,9 +54,9 @@ class HomeDiagrams:
             value=collective_steps,
             title={
                 "text": "Steps", 
-                "font": {"size": 30, "color": PRIMARY_COLOR}
+                "font": {"size": MEDIUM_FONT_SIZE, "color": PRIMARY_COLOR}
             },
-            number={"font": {"size": 50}} # Change color later
+            number={"font": {"size": LARGE_FONT_SIZE}} # Change color later
         ))
 
         fig2 = go.Figure(go.Indicator(
@@ -61,9 +64,9 @@ class HomeDiagrams:
             value=collective_distance,
             title={
                 "text": "Distance", 
-                "font": {"size": 30, "color": PRIMARY_COLOR}
+                "font": {"size": MEDIUM_FONT_SIZE, "color": PRIMARY_COLOR}
             },
-            number={"font": {"size": 50}, "suffix": "km"} # Change color later
+            number={"font": {"size": LARGE_FONT_SIZE}, "suffix": "km"} # Change color later
         ))
 
         fig3 = go.Figure(go.Indicator(
@@ -71,9 +74,9 @@ class HomeDiagrams:
             value=collective_active_minutes,
             title={
                 "text": "Active Minutes", 
-                "font": {"size": 30, "color": PRIMARY_COLOR}
+                "font": {"size": MEDIUM_FONT_SIZE, "color": PRIMARY_COLOR}
             },
-            number={"font": {"size": 50}} # Change color later
+            number={"font": {"size": LARGE_FONT_SIZE}} # Change color later
         ))
 
         fig1.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
@@ -82,3 +85,31 @@ class HomeDiagrams:
 
         return (fig1, fig2, fig3)
 
+    def get_steps_and_active_barplot(self) -> tuple[go.Figure, go.Figure]:
+        df = self.fitbit_db.get_activity_grouped_by_user()
+
+        df = df.sort_values(by="AverageSteps", ascending=False)
+        fig1 = px.bar(
+            df, 
+            x="Id", 
+            y="AverageSteps",
+            title="Average Daily Steps per User - Color Indicating Average Intensity",
+            labels={"Id": "User ID", "AverageSteps": "Steps", "AverageCalories": "Calories"},
+            color="AverageCalories",
+            color_continuous_scale=["#FFFFFF", "#06B0B8"],
+        )
+        fig1.update_xaxes(type='category', tickangle=-45)
+
+        df = df.sort_values(by="AverageActiveMinutes", ascending=False)
+        fig2 = px.bar(
+            df, 
+            x="Id", 
+            y="AverageActiveMinutes",
+            title="Average Daily Steps per User - Color Indicating Average Intensity",
+            labels={"Id": "User ID", "AverageActiveMinutes": "Active Time", "AverageCalories": "Calories"},
+            color="AverageCalories",
+            color_continuous_scale=["#FFFFFF", "#06B0B8"],
+        )
+        fig2.update_xaxes(type='category', tickangle=-45)
+
+        return (fig1, fig2)
--- scripts/dashboard/home_page.py
+++ scripts/dashboard/home_page.py
@@ -10,6 +10,7 @@ home_diagrams = HomeDiagrams(st.session_state["fitbit_db"]) # Change this later
 col1, col2, col3, col4, col5 = st.columns(5)
 left, div, right = st.columns([2, 0.02, 3])
 with left:
+    st.subheader("Survey Details")
     col1, col2 = st.columns(2)
     with col1:
         st.plotly_chart(home_diagrams.get_number_of_days())
@@ -22,13 +23,14 @@ with div:
             <style>
                 .divider-vertical-line {
                     border-left: 2px solid rgba(49, 51, 63, 1);
-                    height: 150px;
+                    height: 200px;
                     margin: auto;
                 }
             </style>
         '''
     )
 with right:
+    st.subheader("Collective Metrics")
     col3, col4, col5 = st.columns(3)
     collective_metrics = home_diagrams.get_collective_metrics()
     with col3:
@@ -37,3 +39,12 @@ with right:
         st.plotly_chart(collective_metrics[1])
     with col5:
         st.plotly_chart(collective_metrics[2])
+
+st.divider()
+
+left, right = st.columns(2)
+bar_plots = home_diagrams.get_steps_and_active_barplot()
+with left:
+    st.plotly_chart(bar_plots[0])
+with right:
+    st.plotly_chart(bar_plots[1])
