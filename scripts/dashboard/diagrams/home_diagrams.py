8b31809c1b6ae10cfec87c93b50c8291bdfec197
Luke Olender <luke.olender@gmail.com>
2025-03-18 18:02:45 +0100

    Created first 3 metrics diagrams

--- scripts/dashboard/diagrams/home_diagrams.py
+++ scripts/dashboard/diagrams/home_diagrams.py
@@ -1,5 +1,97 @@
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
+
 from scripts.database import FitbitDatabase
- 
+
+
 class HomeDiagrams:
     def __init__(self, fitbit_db: FitbitDatabase):
         self.fitbit_db = fitbit_db
+
+    @staticmethod
+    def human_format(num):
+        num = float(f'{num:.2g}')
+        magnitude = 0
+        while abs(num) >= 1000:
+            magnitude += 1
+            num /= 1000.0
+        return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+
+    def get_total_metrics_row(self):
+        num_participants = len(self.fitbit_db.user_ids);
+
+        fig = make_subplots(
+            rows=1, cols=4, 
+            subplot_titles=[
+                "Number of participants",
+                "Research Period", # date range with number of days as subscript
+                "Collective steps taken",
+                "Collective distance travelled",
+                "Collective active time",
+                "Collective calories burned",
+            ], 
+            horizontal_spacing=0.2
+        )
+        
+        fig = go.Figure(go.Indicator(
+            mode="number",
+            value=len(self.fitbit_db.user_ids),
+            title={"text": "Total Participants"},
+            number={"font": {"size": 60, "color": "white"}} # Change color later
+        ))
+
+        return fig
+    
+    def get_number_of_days(self):
+        duration_days = (self.fitbit_db.max_date - self.fitbit_db.min_date).days
+        fig = go.Figure(go.Indicator(
+            mode="number",
+            value=duration_days,
+            title={"text": "Days tracked", "font": {"size": 30, "color": "#84C9FF"}},
+            number={"font": {"size": 50}}#, "suffix": " days"} # Change color later
+        ))
+
+        fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
+
+        return fig
+    
+    def get_number_of_participants(self):
+        fig = go.Figure(go.Indicator(
+            mode="number",
+            value=len(self.fitbit_db.user_ids),
+            title={"text": "Participants", "font": {"size": 30, "color": "#84C9FF"}},
+            number={"font": {"size": 50}} # Change color later
+        ))
+
+        fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
+
+        return fig
+    
+    def get_collective_steps(self):
+        collective_steps_taken = self.fitbit_db.get_daily_steps().loc[:, "TotalSteps"].sum()
+
+        fig = go.Figure(go.Indicator(
+            mode="number",
+            value=collective_steps_taken,
+            title={"text": "Steps", "font": {"size": 30, "color": "#84C9FF"}},
+            number={"font": {"size": 50}} # Change color later
+        ))
+
+        fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
+
+        return fig
+    
+    def get_collective_distance(self):
+        collective_steps_taken = self.fitbit_db.get_daily_steps().loc[:, "TotalSteps"].sum()
+
+        fig = go.Figure(go.Indicator(
+            mode="number",
+            value=collective_steps_taken,
+            title={"text": "Steps", "font": {"size": 30, "color": "#84C9FF"}},
+            number={"font": {"size": 50}} # Change color later
+        ))
+
+        fig.update_layout(height=200, margin=dict(l=0, r=0, t=0, b=0))
+
+        return fig
--- scripts/dashboard/home_page.py
+++ scripts/dashboard/home_page.py
@@ -5,4 +5,34 @@ from scripts.dashboard.diagrams.home_diagrams import HomeDiagrams
 @st.cache_resource
 def get_home_diagrams():
     return HomeDiagrams(st.session_state["fitbit_db"])
-home_diagrams = get_home_diagrams()
+home_diagrams = HomeDiagrams(st.session_state["fitbit_db"]) # Change this later
+
+col1, col2, col3, col4, col5 = st.columns(5)
+left, div, right = st.columns([2, 0.02, 3])
+with left:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.plotly_chart(home_diagrams.get_number_of_days())
+    with col2:
+        st.plotly_chart(home_diagrams.get_number_of_participants())
+with div:
+    st.html(
+        '''
+            <div class="divider-vertical-line"></div>
+            <style>
+                .divider-vertical-line {
+                    border-left: 2px solid rgba(49, 51, 63, 1);
+                    height: 150px;
+                    margin: auto;
+                }
+            </style>
+        '''
+    )
+with right:
+    col3, col4, col5 = st.columns(3)
+    with col3:
+        st.plotly_chart(home_diagrams.get_collective_steps())
+    with col4:
+        pass
+    with col5:
+        pass
