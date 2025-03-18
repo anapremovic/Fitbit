32e0d50cf5a41da3c05e87c5f4e129f422b514cf
Luke Olender <luke.olender@gmail.com>
2025-03-18 14:09:04 +0100

    Undo accidental commit

--- scripts/dashboard/diagrams/home_diagrams.py
+++
@@ -1,5 +0,0 @@
-from scripts.database import FitbitDatabase
-
-class HomeDiagrams:
-    def __init__(self, fitbit_db: FitbitDatabase):
-        self.fitbit_db = fitbit_db
--- scripts/dashboard/home_page.py
+++ scripts/dashboard/home_page.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
 from scripts.database import FitbitDatabase
-from scripts.dashboard.diagrams.home_diagrams import HomeDiagrams
 
-@st.cache_resource
-def get_home_diagrams():
-    return HomeDiagrams(st.session_state["fitbit_db"])
-home_diagrams = get_home_diagrams()
+fitbit_db: FitbitDatabase = st.session_state["fitbit_db"]
