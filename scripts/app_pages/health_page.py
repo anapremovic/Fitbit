a6d3f1741195f0a6197a85dffca120fa2769ad9b
apa109 <apa109@sfu.ca>
2025-03-23 18:36:35 +0100

    Condense health page

--- scripts/app_pages/health_page.py
+++ scripts/app_pages/health_page.py
@@ -25,15 +25,17 @@ def disable_weekdays_toggle():
     if st.session_state["weekends-only"]:
         st.session_state["weekdays-only"] = False
 
-st.subheader("Sleep Duration")
+st.subheader("Sleep")
+
 with st.container(border=True):
-    line, bar = st.columns(2)
+    line, bar, scatter = st.columns([1, 1, 2])
     with line:
         st.plotly_chart(diagrams.get_sleep_duration_over_time_graph())
     with bar:
         st.plotly_chart(diagrams.get_sleep_duration_per_time_blocks_graph())
+    with scatter:
+        st.plotly_chart(diagrams.get_sedentary_hrs_to_sleep_hrs_regression())
 
-st.subheader("What Affects Sleep")
 with st.container(border=True):
     weekdays, weekends = st.columns(2)
     with weekdays:
@@ -48,28 +50,22 @@ with st.container(border=True):
     else:
         st.plotly_chart(diagrams.get_active_hrs_to_sleep_hrs_regression())
 
-with st.container(border=True):
-    st.plotly_chart(diagrams.get_sedentary_hrs_to_sleep_hrs_regression())
+st.subheader("Heart Rate And Calories")
 
-st.subheader("Calories")
 with st.container(border=True):
-    line, bar = st.columns(2)
-    with line:
+    calories_line, calories_bar, heart_rate_line, heart_rate_indicator = st.columns([2, 2, 3, 1])
+
+    with calories_line:
         st.plotly_chart(diagrams.get_calories_burned_over_time_graph())
-    with bar:
+    with calories_bar:
         st.plotly_chart(diagrams.get_calories_burned_per_time_blocks_graph())
 
-st.subheader("Heart Rate")
-heart_rate_over_time, average_heart_rate = diagrams.get_heart_rate_over_time_and_average_graphs()
-with st.container(border=True):
-    plot, number = st.columns([3, 1])
-    with plot:
+    heart_rate_over_time, average_heart_rate = diagrams.get_heart_rate_over_time_and_average_graphs()
+    with heart_rate_line:
         st.plotly_chart(heart_rate_over_time)
-    with number:
+    with heart_rate_indicator:
         st.plotly_chart(average_heart_rate)
 
-weight_over_time = diagrams.get_weight_and_steps_over_time_graphs()
-if weight_over_time:
-    st.subheader("Weight")
-    with st.container(border=True):
-        st.plotly_chart(weight_over_time)
+st.subheader("Weight")
+with st.container(border=True):
+    st.plotly_chart(diagrams.get_weight_and_steps_over_time_graphs())
